cd39d8e730d5fa3dc54f13abd43808de7b5384eb
Jennifer Qi <jeqi@ucsd.edu>
2021-03-04 02:59:18 -0800

    adding/editing JSON next

--- routes/froggy.js
+++ routes/froggy.js
@@ -1,10 +1,38 @@
-var data = require('../myPlantsData.json');
+var myPlantsData = require('../myPlantsData.json');
 
 exports.view = function(req, res){
 	var viewPlant = req.params.plantName;
+	var ind = -1;
+	var accessPic = "edit-photo-button.svg"; 
+	var accessSpecies = "placeholder species";
+	//this may be unneccesary if default image is added when updating json
+
+	for (var i = 0; i < myPlantsData["Plants"].length; i++) {
+		if (myPlantsData["Plants"][i]["nickname"] == viewPlant) {
+			ind = i;
+			break;
+		}
+	}
+
+	//this default safeguard may be unneccesary if default image is added when updating json
+	if (ind != -1) {
+		accessPic = myPlantsData["Plants"][ind]["pic"];
+		accessSpecies = myPlantsData["Plants"][ind]["species"];
+	}
+
+	//calculating age
+	var startDate = new Date(myPlantsData["Plants"][ind]["start"]);
+	var dateCurr = new Date(); //UTC timezone
+
+	var plantAge = dateCurr.getTime() - startDate.getTime();
+	var ageDays = plantAge/(1000 * 3600 * 24);
+	ageDays = Math.floor(ageDays);
 
   res.render('froggy', {
-  	"nickname": viewPlant
+  	"nickname": viewPlant,
+  	"pic": accessPic,
+  	"species": accessSpecies,
+  	"age": ageDays
   });
 };
 
--- routes/myPlantsPage.js
+++ routes/myPlantsPage.js
@@ -1,22 +1,23 @@
 /*
  * GET home page.
  */
-var data = require("../myPlantsData.json")
+var myPlantsData = require("../myPlantsData.json")
 
-	var startDate = new Date(data.startDate);
+	var startDate = new Date(myPlantsData.startDate);
 	var dateCurr = new Date();
 
 	var plantAge = dateCurr.getTime() - startDate.getTime();
 	var ageDays = plantAge/(1000 * 3600 * 24);
+	ageDays = Math.floor(ageDays);
 
 exports.view = function(req, res){
   res.render('myPlantsPage', {
-  		"pic": data.pic,
-  		"nickname": data.nickname,
-		"species": data.species,
-		"age": data.age,
-		"watering": data.watering,
-		"Plants": data.Plants,
+  		"pic": myPlantsData.pic,
+  		"nickname": myPlantsData.nickname,
+		"species": myPlantsData.species,
+		"age": myPlantsData.age,
+		"watering": myPlantsData.watering,
+		"Plants": myPlantsData.Plants,
 		"autoAge": ageDays
   });
 };
